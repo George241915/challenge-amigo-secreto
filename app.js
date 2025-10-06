9805213d47c23fbdf276dcfb7dc957262c868d4d
Jorge Erazo <george241915@gmail.com>
2025-10-05 21:06:29 -0500

    Final

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// creacion de variables
+let amigos = [];
+let amigoSecreto = '';
+let amigoSorteado = '';
+
